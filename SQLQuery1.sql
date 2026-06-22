d23533c40b4d7d23c9043c4ae76d49ca1bf76aba
MAndresPicco <142451486+MAndresPicco@users.noreply.github.com>
2026-06-22 20:27:54 -0300

    Add files via upload
    Entrega 1, Modulo 3

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,34 +1,22 @@
 --CREAR BASE DE DATOS
-CREATE DATABASE Ventas; 
+CREATE DATABASE Ventas;
 
--- Clausula USE
+--Clausula USE
 USE Ventas;
 
--- CREAR TABLA CLIENTES
-CREATE TABLE Clientes(
+--CREAR TABLA CLIENTES
+CREATE TABLE Cliente (
 id_cliente int NOT NULL IDENTITY (1,1) PRIMARY KEY,
-NombreCliente varchar(50) NOT NULL,
-Direccion varchar(50) NOT NULL,
-Ciudad varchar(50) NOT NULL,
-Provincia text NOT NULL,
-Pais text NOT NULL,
+nombrecliente varchar(100) NOT NULL,
+perfil_bio text NOT NULL,
+fecha_registro DATE 
 );
 
-SELECT * FROM Clientes;
 
 --CREAR TABLA PRODUCTOS
-CREATE TABLE Productos(
-id_producto INT,
-nombre_producto varchar(100),
-precio DECIMAL(10, 2),
-fecha_ingreso DATE,
-esta_activo INT
-);
-
-SELECT * FROM Productos;
-
-
-
-
-
-
+CREATE TABLE Producto (
+id_producto INT PRIMARY KEY,
+nombre_producto varchar(100) NOT NULL,
+precio DECIMAL(10,2) NOT NULL,
+fecha_ingreso DATE, 
+esta_activo INT NOT NULL DEFAULT 1
